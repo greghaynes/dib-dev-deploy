71756cb04bf918d85894b58a33e673c4c01baeb3
Gregory Haynes <greg@greghaynes.net>
2015-02-05 01:00:32 -0800

    Added vm defining

--- dib-dev-deploy.sh
+++ dib-dev-deploy.sh
@@ -2,11 +2,19 @@
 set -eux
 set -o pipefail
 
-export DDD_WORKDIR=${DDD_WORKDIR:-~/ddd-workdir}
-image_name="test"
+source $(which ddd-env-vars)
 
 mkdir -p $DDD_WORKDIR
 
+if ! [ -f "$DDD_VM_TEMPLATE" ]; then
+    echo "Could not find vm template at $DDD_VM_TEMPLATE."
+    exit 1
+fi
+
+name="test"
+
 ddd-pull-tools
 
-ddd-create-image -a amd64 -o $DDD_WORKDIR/images/$image_name $*
+ddd-create-image -a amd64 -o $DDD_VM_IMAGES_DIR/$name.qcow2 $*
+
+ddd-define-vm $name1 $name
